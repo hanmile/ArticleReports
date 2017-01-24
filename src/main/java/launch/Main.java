6d6ba77f1ddb1c4e3e16999b94a5b6ab7dd9c4fa
hanmile <hans.petter.mile@schibsted.no>
2017-01-24 20:56:27 +0100

    Second commit

---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,7 @@
+package main.java.launch;
+
+/**
+ * Created by hanmile on 24.01.2017.
+ */
+public class Main {
+}
---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,7 @@
+package main.java.servlet;
+
+/**
+ * Created by hanmile on 24.01.2017.
+ */
+public class HelloServlet {
+}
