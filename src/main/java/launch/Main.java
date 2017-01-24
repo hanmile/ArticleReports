794ae224324057ee769f9e57c315b997549af4e1
hanmile <hans.petter.mile@schibsted.no>
2017-01-24 22:07:26 +0100

    After build

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,12 +1,13 @@
 package main.java.launch;
 
-import java.io.File;
 
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
 
 public class Main {
 
