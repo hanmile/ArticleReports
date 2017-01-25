2094607a87a34d1946bf87ff955aeb3866da4b63
hanmile <hans.petter.mile@schibsted.no>
2017-01-25 21:55:49 +0100

    Added springboot

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -9,7 +9,7 @@ import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
 
-public class Main_old {
+public class Main {
 
     public static void main(String[] args) throws Exception {
 
